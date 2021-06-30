99657c77915ffd13b940a7b2fa7dd6fe83930197
jobn123 <jobn_joy@163.com>
2021-06-30 22:05:41 +0800

    :sparkles: 多进程管理

--- models/command/lib/index.js
+++ models/command/lib/index.js
@@ -2,15 +2,33 @@
 const semver = require('semver');
 const colors = require('colors/safe');
 
+const log = require('@tyo-cli/log');
+
 const LOWEST_NODE_VERSION = '13.0.0';
 
 class Command {
     constructor(argv) {
-        console.log('>>>>>Command constructor');
+
+        if (!argv || argv.length < 1) {
+            throw new Error('参数不能为空')
+        }
+
+        if (!Array.isArray(argv)) {
+            throw new Error('参数必须为数组')
+        }
+
         this._argv = argv;
+
         let runner = new Promise((resolve, reject) => {
             let chain = Promise.resolve();
             chain = chain.then(() => this.checkNodeVersion());
+            chain = chain.then(() => this.initArgs());
+            chain = chain.then(() => this.init());
+            chain = chain.then(() => this.exec());
+            chain.catch(err => {
+                // console.log(err.message);
+                log.error(err.message);
+            })
         })
     }
 
@@ -19,6 +37,11 @@ class Command {
         if (!semver.gte(CURRENT_NODE_VERSION, LOWEST_NODE_VERSION)) {
             throw new Error(colors.red(`tyo-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的Node.js`));
         }
+    }
+
+    initArgs() {
+        this._cmd = this._argv[this._argv.length - 2];
+        this._argv = this._argv.slice(0, this._argv.length - 1);
     }
 
     init() {
